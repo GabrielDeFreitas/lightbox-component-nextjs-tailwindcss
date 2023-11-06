84580636af09867591bb4892eeafef890ef9c5fd
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-06 14:31:19 -0300

    create thumbImage component

---
+++ src/components/thumbImage.tsx
@@ -0,0 +1,24 @@
+import Image, { StaticImageData } from "next/image";
+
+interface ThumbImageProps {
+    srcImage: StaticImageData,
+    onClick: () => void
+
+}
+
+export function ThumbImage({srcImage, onClick} : ThumbImageProps) {
+    return (
+        <button
+          onClick={onClick}
+          className="w-full h-full overflow-hidden rounded-md"
+        >
+            <Image
+                src={srcImage}
+                width={300}
+                height={200}
+                alt="image"
+                className="hover:scale-110 transition-all ease-linear cursor-pointer"
+            />
+        </button>
+    )
+}
