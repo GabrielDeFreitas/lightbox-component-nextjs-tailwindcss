414fdbf4cde9de30889ebb24874a30e6559bc0c8
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-06 22:03:03 -0300

    create function navegate img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,17 @@ import Image06 from '@/assets/photo06.avif'
 import { ThumbImage } from '@/components/thumbImage'
 import Arrow from '@/assets/arrow.svg'
 import Close from '@/assets/close.svg'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { useState } from 'react'
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+
+  const [indexCurrent, setIndexCurrent] = useState(0)
+
+  const [dataImgCurrent, setDataImgCurrent] = useState <StaticImageData | null> (null);
+
+  const isOpen = dataImgCurrent !== null
+
   const listImages = [
     Image01,
     Image02,
@@ -24,65 +30,88 @@ export default function Home() {
   ]
 
   function handleClickOpenImage (index: number) {
-    console.log(index)
-    setIsOpen(true)
+    setIndexCurrent(index)
+    setDataImgCurrent(listImages[index])
   }
 
   function handleCloseLightBox () {
-    setIsOpen(false)
+    setDataImgCurrent(null)
+  }
+
+  function handleNavigate(direction : 'next' | 'previous') {
+    let index = direction == 'next' ? indexCurrent + 1 : indexCurrent -1
+
+    console.log(index)
+
+    if(index < 0) {
+      index = listImages.length - 1
+    }
+
+    if(index >= listImages.length) {
+      index = 0
+    }
+
+    setIndexCurrent(index)
+
+    setDataImgCurrent(listImages[index])
   }
 
   return (
     <>
-      <section className='w-full min-h-screen flex items-start justify-center py-10 px-3 bg-slate-100'>
-        <div className='grid grid-cols-3 gap-3'>
-          {
-            listImages.map((img, index) =>(
-              <ThumbImage
-                key={index}
-                srcImage={img}
-                onClick={() => handleClickOpenImage(index)}
-              />
-            ))
-          }
-        </div>
-      </section>
-      {
-        isOpen && (
-          <div className='fixed inset-0 w-full h-full z-30 flex items-center justify-center bg-black bg-opacity-50 gap-4'>
-            <button onClick={handleCloseLightBox} className='absolute top-5 right-5 w-10 hover:opacity-50 transition-opacity'>
-              <Image
-                src={Close}
-                alt='Button close'
-              />
-            </button>
-            <button className='w-12 rotate-180 hover:opacity-50 transition-opacity'>
-              <Image
-                src={Arrow}
-                alt='arrow previous'
-              />
-            </button>
+      <main className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <section className='w-full min-h-screen flex items-start justify-center py-6 px-3'>
+          <div className='grid grid-cols-3 gap-3'>
             {
-              <Image
-                src={Image01}
-                width={700}
-                height={450}
-                //fill
-                //sizes='(max-width: 760px) 100vw, 700px'
-                priority
-                alt='image'
-              />
+              listImages.map((img, index) =>(
+                <ThumbImage
+                  key={index}
+                  srcImage={img}
+                  onClick={() => handleClickOpenImage(index)}
+                />
+              ))
             }
-            <button className='w-12 hover:opacity-50 transition-opacity'>
-              <Image
-                src={Arrow}
-                alt='arrow next'
-                width={500}
-              />
-            </button>
           </div>
-        )
-      }
+        </section>
+        {
+          isOpen && (
+            <div className='fixed inset-0 w-full h-full z-30 flex items-center justify-center bg-black bg-opacity-50 gap-4'>
+              <button onClick={handleCloseLightBox} className='absolute top-5 right-5 w-10 hover:opacity-50 transition-opacity'>
+                <Image
+                  src={Close}
+                  alt='Button close'
+                />
+              </button>
+              <button className='w-12 rotate-180 hover:opacity-50 transition-opacity'>
+                <Image
+                  src={Arrow}
+                  alt='arrow previous'
+                  onClick={() => handleNavigate('previous')}
+                />
+              </button>
+              {
+                dataImgCurrent &&
+                <Image
+                  src={dataImgCurrent}
+                  width={700}
+                  height={450}
+                  quality={40}
+                  //fill
+                  //sizes='(max-width: 760px) 100vw, 700px'
+                  alt='image'
+                />
+              }
+              <button className='w-12 hover:opacity-50 transition-opacity'>
+                <Image
+                  src={Arrow}
+                  alt='arrow next'
+                  width={500}
+                  onClick={() => handleNavigate('next')}
+                />
+              </button>
+            </div>
+          )
+        }
+      </main>
     </>
   )
 }
--- src/components/thumbImage.tsx
+++ src/components/thumbImage.tsx
@@ -1,24 +1,23 @@
 import Image, { StaticImageData } from "next/image";
-
 interface ThumbImageProps {
-    srcImage: StaticImageData,
-    onClick: () => void
-
+  srcImage: StaticImageData,
+  onClick: () => void
 }
 
 export function ThumbImage({srcImage, onClick} : ThumbImageProps) {
-    return (
-        <button
-          onClick={onClick}
-          className="w-full h-full overflow-hidden rounded-md"
-        >
-            <Image
-                src={srcImage}
-                width={300}
-                height={200}
-                alt="image"
-                className="hover:scale-110 transition-all ease-linear cursor-pointer"
-            />
-        </button>
-    )
+  return (
+    <button
+      onClick={onClick}
+      className="w-full h-full overflow-hidden rounded-md"
+    >
+    <Image
+      src={srcImage}
+      width={300}
+      height={200}
+      quality={20}
+      alt="image"
+      className="hover:scale-110 transition-all ease-linear cursor-pointer"
+    />
+    </button>
+  )
 }
